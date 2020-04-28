76e220f9b69b794e5290fce47ca4bcc1aec960e8
Vojtěch Janoušek <janousek315@gmail.com>
2020-04-28 09:16:51 +0200

    Changed server structure for pytest

--- link_shortener/server.py
+++ link_shortener/server.py
@@ -18,41 +18,10 @@ from link_shortener.view_routes import view_blueprint
 from link_shortener.api.retrieve import api_retrieve_blueprint
 
 
-app = Sanic(__name__)
-
-# ----------------------------------------------------------------------------
-# CONFIGURATION
-# ----------------------------------------------------------------------------
-
-app.blueprint(initdb_blueprint)
-app.blueprint(oauth_blueprint)
-app.blueprint(auth_blueprint)
-app.blueprint(form_blueprint)
-app.blueprint(view_blueprint)
-app.blueprint(api_retrieve_blueprint)
-
-app.static('/links/', './static/')
-app.config.WTF_CSRF_SECRET_KEY = config('WTF_CSRF_SECRET_KEY')
-
-# ----------------------------------------------------------------------------
-# AUTHENTICATION
-# ----------------------------------------------------------------------------
-
-app.session_interface = InMemorySessionInterface()
-
-app.config.OAUTH_PROVIDER = config('OAUTH_PROVIDER')
-app.config.OAUTH_REDIRECT_URI = config('OAUTH_REDIRECT_URI')
-app.config.OAUTH_SCOPE = config('OAUTH_SCOPE')
-app.config.OAUTH_CLIENT_ID = config('OAUTH_CLIENT_ID')
-app.config.OAUTH_CLIENT_SECRET = config('OAUTH_CLIENT_SECRET')
-
-
-@app.middleware('request')
 async def add_session_to_request(request):
     await request.app.session_interface.open(request)
 
 
-@app.middleware('response')
 async def save_session(request, response):
     '''
     See SESSION_ERROR in Documentation.
@@ -75,5 +44,41 @@ async def save_session(request, response):
     await request.app.session_interface.save(request, response)
 
 
+def create_app():
+    app = Sanic(__name__)
+
+    # ----------------------------------------------------------------------------
+    # CONFIGURATION
+    # ----------------------------------------------------------------------------
+
+    app.blueprint(initdb_blueprint)
+    app.blueprint(oauth_blueprint)
+    app.blueprint(auth_blueprint)
+    app.blueprint(form_blueprint)
+    app.blueprint(view_blueprint)
+    app.blueprint(api_retrieve_blueprint)
+
+    app.static('/links/', './static/')
+    app.config.WTF_CSRF_SECRET_KEY = config('WTF_CSRF_SECRET_KEY')
+
+    # ----------------------------------------------------------------------------
+    # AUTHENTICATION
+    # ----------------------------------------------------------------------------
+
+    app.session_interface = InMemorySessionInterface()
+
+    app.config.OAUTH_PROVIDER = config('OAUTH_PROVIDER')
+    app.config.OAUTH_REDIRECT_URI = config('OAUTH_REDIRECT_URI')
+    app.config.OAUTH_SCOPE = config('OAUTH_SCOPE')
+    app.config.OAUTH_CLIENT_ID = config('OAUTH_CLIENT_ID')
+    app.config.OAUTH_CLIENT_SECRET = config('OAUTH_CLIENT_SECRET')
+
+    app.register_middleware(add_session_to_request, 'request')
+    app.register_middleware(save_session, 'response')
+
+    return app
+
+
 if (__name__ == '__main__'):
+    app = create_app()
     app.run(host='0.0.0.0', port=8000)
--- link_shortener/tests/test_simple_views.py
+++ link_shortener/tests/test_simple_views.py
@@ -1,7 +1,19 @@
 import pytest
-from link_shortener.server import app
+
+from unittest import TestCase
+
+from link_shortener.server import create_app
 
 
-def test_about_page():
-    request, response = app.test_client.get('/links/about')
-    assert response.status == 200
+class TestViews(TestCase):
+
+    def setUp(self):
+        self.app = create_app()
+
+    def test_about_page(self):
+        request, response = self.app.test_client.get('/links/about')
+        self.assertEqual(response.status, 200)
+
+    def test_landing_page(self):
+        request, response = self.app.test_client.get('/')
+        self.assertEqual(response.status, 200)

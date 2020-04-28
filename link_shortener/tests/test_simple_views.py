79adaf34bff910574aba70d5c74b1e00de4c1351
Vojtěch Janoušek <janousek315@gmail.com>
2020-04-28 10:30:15 +0200

    Added tests for simple view routes

--- link_shortener/tests/test_simple_views.py
+++ link_shortener/tests/test_simple_views.py
@@ -10,10 +10,42 @@ class TestViews(TestCase):
     def setUp(self):
         self.app = create_app()
 
-    def test_about_page(self):
-        request, response = self.app.test_client.get('/links/about')
+    def test_getting_about_page(self):
+        '''
+        Test that getting the about page is successful.
+        '''
+        response = self.app.test_client.get(
+            '/links/about',
+            gather_request=False
+        )
         self.assertEqual(response.status, 200)
 
-    def test_landing_page(self):
-        request, response = self.app.test_client.get('/')
+    def test_getting_landing_page(self):
+        '''
+        Test that getting the landing page is successful.
+        '''
+        response = self.app.test_client.get(
+            '/',
+            gather_request=False
+        )
         self.assertEqual(response.status, 200)
+
+    def test_existing_endpoint_redirect(self):
+        '''
+        Test that using an existing endpoint is successful.
+        '''
+        response = self.app.test_client.get(
+            '/vlk',
+            gather_request=False
+        )
+        self.assertEqual(response.status, 200)
+
+    def test_wrong_endpoint_redirect(self):
+        '''
+        Test that using a non-existing endpoint fails.
+        '''
+        response = self.app.test_client.get(
+            '/nevlk',
+            gather_request=False
+        )
+        self.assertEqual(response.status, 400)

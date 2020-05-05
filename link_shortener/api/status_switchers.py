e26edf32ef839d4b0a83d52812ee011e383ddca4
Vojtěch Janoušek <janousek315@gmail.com>
2020-05-05 20:02:12 +0200

    Deactivate due links API method

--- link_shortener/api/status_switchers.py
+++ link_shortener/api/status_switchers.py
@@ -3,6 +3,7 @@ Copyright (C) 2020 Link Shortener Authors (see AUTHORS in Documentation).
 Licensed under the MIT (Expat) License (see LICENSE in Documentation).
 '''
 from datetime import date
+from decouple import config
 
 from sanic import Blueprint
 from sanic.response import json
@@ -49,7 +50,7 @@ async def activate_due_links(request):
             link_data = await queryset.fetchall()
             if not link_data:
                 await trans.close()
-                return json({'message': 'Nothing to activate'}, status=200)
+                return json({'message': 'No links to activate'}, status=200)
 
             count = 0
             for link in link_data:
@@ -94,4 +95,76 @@ async def activate_due_links(request):
 
 @api_switcher_blueprint.route('/api/status/deactivate', methods=['POST'])
 async def deactivate_due_links(request):
-    pass
+    try:
+        token = request.headers['Bearer']
+        if (token != config('ACCESS_TOKEN')):
+            return json({'message': 'Unauthorized'}, status=401)
+
+    except KeyError:
+        return json({'message': 'Please provide a token'}, status=400)
+
+    try:
+        year = request.data['Year']
+        month = request.data['Month']
+        day = request.data['Day']
+
+        switch_date = date(year, month, day)
+
+    except KeyError:
+        return json({'message': 'Please provide the date'}, status=400)
+
+    except Exception:
+        return json({'message': 'Incorrect payload'}, status=400)
+
+    try:
+        async with request.app.engine.acquire() as conn:
+            trans = await conn.begin()
+            queryset = await conn.execute(
+                actives.select().where(
+                    actives.columns['switch_date'] == switch_date
+                )
+            )
+            link_data = await queryset.fetchall()
+            if not link_data:
+                await trans.close()
+                return json({'message': 'No links to deactivate'}, status=200)
+
+            count = 0
+            for link in link_data:
+                count += 1
+                await conn.execute(
+                    inactives.insert().from_select(
+                        [
+                            'identifier',
+                            'owner',
+                            'owner_id',
+                            'password',
+                            'endpoint',
+                            'url'
+                        ],
+                        sql_select([
+                            actives.c.identifier,
+                            actives.c.owner,
+                            actives.c.owner_id,
+                            actives.c.password,
+                            actives.c.endpoint,
+                            actives.c.url
+                        ]).where(actives.c.id == link.id)
+                    )
+                )
+                await conn.execute(
+                    actives.delete().where(
+                        actives.columns['id'] == link.id
+                    )
+                )
+
+            await trans.commit()
+            await trans.close()
+            return json(
+                {'message': '{} links successfully deactivated'.format(count)},
+                status=200
+            )
+
+    except Exception:
+        await trans.close()
+        return json({'message': 'Deactivating links failed'}, status=500)

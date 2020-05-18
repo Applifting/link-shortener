6b0c5702ee178b4f08ea3cd7148084ddbc7a52a2
Vojtěch Janoušek <janousek315@gmail.com>
2020-05-18 13:28:50 +0200

    Delete by identifier API method

--- link_shortener/api/create_delete.py
+++ link_shortener/api/create_delete.py
@@ -125,3 +125,52 @@ async def api_delete_link_by_id(request, status, link_id):
     except Exception:
         await trans.close()
         return json({'message': 'Deleting link failed'}, status=500)
+
+
+@api_create_delete_blueprint.route(
+    '/api/link/<identifier>',
+    methods=['DELETE']
+)
+async def api_delete_link_by_identifier(request, identifier):
+    try:
+        token = request.headers['Bearer']
+        if (token != config('ACCESS_TOKEN')):
+            return json({'message': 'Unauthorized'}, status=401)
+
+    except KeyError:
+        return json({'message': 'Please provide a token'}, status=400)
+
+    try:
+        async with request.app.engine.acquire() as conn:
+            trans = await conn.begin()
+            ac_queryset = await conn.execute(
+                actives.select().where(
+                    actives.columns['identifier'] == identifier
+                )
+            )
+            link_data = await ac_queryset.fetchone()
+            table = actives
+            if not link_data:
+                in_queryset = await conn.execute(
+                    inactives.select().where(
+                        inactives.columns['identifier'] == identifier
+                    )
+                )
+                link_data = await in_queryset.fetchone()
+                table = inactives
+                if not link_data:
+                    await trans.close()
+                    return json({'message': 'Link does not exist'}, status=404)
+
+            await conn.execute(
+                table.delete().where(
+                    table.columns['identifier'] == link_data.identifier
+                )
+            )
+            await trans.commit()
+            await trans.close()
+            return json({}, status=204)
+
+    except Exception:
+        await trans.close()
+        return json({'message': 'Deleting link failed'}, status=500)

ba6d27002ffaf51b5ea4f309684a3d307e6c076b
Vojtěch Janoušek <janousek315@gmail.com>
2020-05-26 09:34:14 +0200

    Reformed link models

--- link_shortener/models.py
+++ link_shortener/models.py
@@ -2,25 +2,13 @@
 Copyright (C) 2020 Link Shortener Authors (see AUTHORS in Documentation).
 Licensed under the MIT (Expat) License (see LICENSE in Documentation).
 '''
-from sqlalchemy import MetaData, Table, Column, String, Integer, BLOB, Date
+from sqlalchemy import MetaData, Table, Column, String, Integer, BLOB,
+                        Date, Boolean
 
 
 metadata = MetaData()
-actives = Table(
-    'active_links',
-    metadata,
-    Column('id', Integer, primary_key=True, autoincrement=True),
-    Column('identifier', String(36)),
-    Column('owner', String(50)),
-    Column('owner_id', String(255)),
-    Column('password', BLOB, default=None),
-    Column('endpoint', String(20), unique=True),
-    Column('url', String(300)),
-    Column('switch_date', Date, default=None)
-)
-
-inactives = Table(
-    'inactive_links',
+links = Table(
+    'links',
     metadata,
     Column('id', Integer, primary_key=True, autoincrement=True),
     Column('identifier', String(36)),
@@ -29,8 +17,22 @@ inactives = Table(
     Column('password', BLOB, default=None),
     Column('endpoint', String(20)),
     Column('url', String(300)),
-    Column('switch_date', Date, default=None)
+    Column('switch_date', Date, default=None),
+    Column('is_active', Boolean, default=False)
 )
+
+# inactives = Table(
+#     'inactive_links',
+#     metadata,
+#     Column('id', Integer, primary_key=True, autoincrement=True),
+#     Column('identifier', String(36)),
+#     Column('owner', String(50)),
+#     Column('owner_id', String(255)),
+#     Column('password', BLOB, default=None),
+#     Column('endpoint', String(20)),
+#     Column('url', String(300)),
+#     Column('switch_date', Date, default=None)
+# )
 
 salts = Table(
     'hash_salts',

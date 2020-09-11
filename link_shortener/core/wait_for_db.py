c00256a0a0f77e94ba4ddc51963dccd40ac09307
MAL <maciej.laskowski@applifting.cz>
2020-09-11 11:26:13 +0200

    Removed 12sec sleeping. Added test for DB connection establishment.

--- link_shortener/core/wait_for_db.py
+++ link_shortener/core/wait_for_db.py
@@ -4,5 +4,39 @@ Licensed under the MIT (Expat) License (see LICENSE in Documentation).
 '''
 import time
 
+from decouple import config
+from sqlalchemy import create_engine
+from sqlalchemy.exc import OperationalError
 
-time.sleep(12)
+
+def test_db():
+    try:
+        if config('PRODUCTION', default=False, cast=bool):
+            engine = create_engine(
+                f"mysql+pymysql://"
+                f"{config('MYSQL_USER')}:"
+                f"{config('MYSQL_PASSWORD')}@"
+                f"{config('MYSQL_HOST')}:"
+                f"{config('MYSQL_PORT', cast=int)}/"
+                f"{config('MYSQL_DB')}"
+            )
+
+        else:
+            engine = create_engine(
+                "mysql+pymysql://user:password@db:3306/db"
+            )
+
+        engine.begin()
+
+    except OperationalError:
+        print("Waiting for DB to load.")
+        time.sleep(2)
+        test_db()
+
+    except Exception as e:
+        raise Exception(e)
+
+    print("DB loaded. Starting up the server")
+
+
+test_db()

b5a95ccc54efaa1efc24442929685846705e62f0
MAL <maciej.laskowski@applifting.cz>
2020-09-15 14:16:26 +0200

    variable edit in filter.py. link_data removed

--- link_shortener/core/filter.py
+++ link_shortener/core/filter.py
@@ -4,9 +4,8 @@ Licensed under the MIT (Expat) License (see LICENSE in Documentation).
 '''
 
 
-def filter_links(link_data, filters):
-    owners = set(row['owner'] for row in link_data)
-    data = link_data
+def filter_links(data, filters):
+    owners = set(row['owner'] for row in data)
 
     if filters['owner']:
         data = [row for row in data if row['owner'] == filters['owner']]

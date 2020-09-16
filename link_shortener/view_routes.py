515d349b1310c996b6102f17967d11bbf489074f
MAL <maciej.laskowski@applifting.cz>
2020-09-16 16:24:08 +0200

    view_routes.py all_active_links() code reformat

--- link_shortener/view_routes.py
+++ link_shortener/view_routes.py
@@ -3,27 +3,21 @@ Copyright (C) 2020 Link Shortener Authors (see AUTHORS in Documentation).
 Licensed under the MIT (Expat) License (see LICENSE in Documentation).
 '''
 from decouple import config
-
+from prometheus_client import Counter, generate_latest
 from sanic import Blueprint
 from sanic.response import html, redirect, text, json
-
 from sanic_oauth.blueprint import login_required
 
-from prometheus_client import Counter, generate_latest
-
-from link_shortener.templates import template_loader
-
-from link_shortener.commands.retrieve import retrieve_links
-from link_shortener.commands.update import reset_password
-from link_shortener.commands.switch import activate_link, deactivate_link
 from link_shortener.commands.delete import delete_link
 from link_shortener.commands.redirect import redirect_link
-
+from link_shortener.commands.retrieve import retrieve_links
+from link_shortener.commands.switch import activate_link, deactivate_link
+from link_shortener.commands.update import reset_password
+from link_shortener.core.decorators import credential_whitelist_check
 from link_shortener.core.exceptions import (DuplicateActiveLinkForbidden,
                                             NotFoundException)
-from link_shortener.core.decorators import credential_whitelist_check
 from link_shortener.core.filter import filter_links, get_filter_dict
-
+from link_shortener.templates import template_loader
 
 view_blueprint = Blueprint('views')
 redirect_counter = Counter(
@@ -49,10 +43,10 @@ async def redirect_link_view(request, link_endpoint):
         return redirect(target, status=307)
     except NotFoundException:
         return html(template_loader(
-                        template_file='message.html',
-                        payload='Link inactive or does not exist',
-                        status_code='404'
-                    ), status=404)
+            template_file='message.html',
+            payload='Link inactive or does not exist',
+            status_code='404'
+        ), status=404)
 
 
 @view_blueprint.route('/', methods=['GET'])
@@ -71,15 +65,18 @@ async def about_page(request):
 async def all_active_links(request, user):
     filters = get_filter_dict(request)
 
-    link_data = await retrieve_links(request,
-                                     {'is_active': filters["is_active"]})
+    link_data = await retrieve_links(
+        request,
+        {'is_active': filters['is_active']}
+    )
+
     filtered_data = filter_links(link_data, filters)
 
     return html(template_loader(
-                    template_file='all_links.html',
-                    domain_name=config('DOMAIN_NAME'),
-                    data=filtered_data
-                ), status=200)
+        template_file='all_links.html',
+        domain_name=config('DOMAIN_NAME'),
+        data=filtered_data
+    ), status=200)
 
 
 @view_blueprint.route('/links/me', methods=['GET'])
@@ -88,10 +85,10 @@ async def all_active_links(request, user):
 async def owner_specific_links(request, user):
     link_data = await retrieve_links(request, filters={'owner_id': user.id})
     return html(template_loader(
-                    template_file='my_links.html',
-                    domain_name=config('DOMAIN_NAME'),
-                    link_data=link_data
-                ), status=200)
+        template_file='my_links.html',
+        domain_name=config('DOMAIN_NAME'),
+        link_data=link_data
+    ), status=200)
 
 
 @view_blueprint.route('/delete/<link_id>', methods=['GET'])
@@ -105,10 +102,10 @@ async def delete_link_view(request, user, link_id):
         status, message = 404, 'Link does not exist'
     finally:
         return html(template_loader(
-                        template_file='message.html',
-                        payload=message,
-                        status_code=str(status)
-                    ), status=status)
+            template_file='message.html',
+            payload=message,
+            status_code=str(status)
+        ), status=status)
 
 
 @view_blueprint.route('/delete/<link_id>/confirm', methods=['GET'])
@@ -116,8 +113,8 @@ async def delete_link_view(request, user, link_id):
 @credential_whitelist_check
 async def confirm_delete_link_view(request, user, link_id):
     return html(template_loader(
-                    template_file='delete_link.html',
-                    link_id=link_id), status=200)
+        template_file='delete_link.html',
+        link_id=link_id), status=200)
 
 
 @view_blueprint.route('/activate/<link_id>', methods=['GET'])
@@ -133,10 +130,10 @@ async def activate_link_view(request, user, link_id):
         status, message = 400, 'An active link with that name already exists'
     finally:
         return html(template_loader(
-                        template_file='message.html',
-                        payload=message,
-                        status_code=str(status)
-                    ), status=status)
+            template_file='message.html',
+            payload=message,
+            status_code=str(status)
+        ), status=status)
 
 
 @view_blueprint.route('/deactivate/<link_id>', methods=['GET'])
@@ -150,10 +147,10 @@ async def deactivate_link_view(request, user, link_id):
         status, message = 404, 'Link does not exist'
     finally:
         return html(template_loader(
-                        template_file='message.html',
-                        payload=message,
-                        status_code=str(status)
-                    ), status=status)
+            template_file='message.html',
+            payload=message,
+            status_code=str(status)
+        ), status=status)
 
 
 @view_blueprint.route('/reset/<link_id>', methods=['GET'])
@@ -167,7 +164,7 @@ async def reset_password_view(request, user, link_id):
         status, message = 404, 'Link has no password or does not exist'
     finally:
         return html(template_loader(
-                        template_file='message.html',
-                        payload=message,
-                        status_code=str(status)
-                    ), status=status)
+            template_file='message.html',
+            payload=message,
+            status_code=str(status)
+        ), status=status)

9ec275cf79802b1c4da9f79082e9df7f31cf8484
MAL <maciej.laskowski@applifting.cz>
2020-10-01 17:17:36 +0200

    code review  fixes

--- link_shortener/templates.py
+++ link_shortener/templates.py
@@ -3,7 +3,6 @@ Copyright (C) 2020 Link Shortener Authors (see AUTHORS in Documentation).
 Licensed under the MIT (Expat) License (see LICENSE in Documentation).
 '''
 from decouple import config
-
 from jinja2 import Environment, PackageLoader
 
 
@@ -14,6 +13,10 @@ def template_loader(template_file, *args, **kwargs):
     file_loader = PackageLoader(__name__, 'templates')
     env = Environment(loader=file_loader)
     template = env.get_template(template_file)
-    ga_id = config('GOOGLE_ANALYTICS_ID')
-    output = template.render(*args, **kwargs, ga_id=ga_id)
+    analytics_id = config('ANALYTICS_ID')
+    analytics_dsn = config('ANALYTICS_DSN')
+    output = template.render(*args, **kwargs,
+                             analytics_id=analytics_id,
+                             analytics_dsn=analytics_dsn
+                             )
     return output

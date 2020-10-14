adbcb6b97fc4f1f049dbdb3eebb47d19013fe45e
Vojtěch Janoušek <vojtech.janousek@applifting.cz>
2020-10-14 10:52:34 +0200

    Redirect not-found exceptions to its new template

--- link_shortener/form_routes.py
+++ link_shortener/form_routes.py
@@ -67,11 +67,7 @@ async def link_password_form(request, link_id):
                         payload=data
                     ), status=200)
     except NotFoundException:
-        return html(template_loader(
-                        template_file='message.html',
-                        payload='Link has no password or does not exist',
-                        status_code='404'
-                    ), status=404)
+        return html(template_loader('message.html'), status=404)
 
 
 @form_blueprint.route('/authorize/<link_id>', methods=['POST'])
@@ -83,11 +79,10 @@ async def link_password_save(request, link_id):
                         template_file='redirect.html',
                         link=link,
                     ), status=307)
-
     except FormInvalidException:
         message = 'invalid-form'  # status = 400
     except NotFoundException:
-        message = 'not-found'  # status = 404
+        return html(template_loader('message.html'), status=404)
     except AccessDeniedException:
         message = 'incorrect-password'  # status = 401
 
@@ -148,8 +143,7 @@ async def update_link_form(request, user, link_id):
                         default_password=config('DEFAULT_PASSWORD')
                     ), status=200)
     except NotFoundException:
-        params = '?origin=edit&status=not-found'
-        return redirect(f'/links/all{params}')
+        return html(template_loader('message.html'), status=404)
 
 
 @form_blueprint.route('/edit/<link_id>', methods=['POST'])
@@ -172,9 +166,8 @@ async def update_link_save(request, user, link_id):
     except FormInvalidException:
         message = 'form-invalid'  # status = 400
     except NotFoundException:
-        message = 'not-found'  # status = 404
+        return html(template_loader('message.html'), status=404)
     except DuplicateActiveLinkForbidden:
         message = 'duplicate'  # status = 409
-    finally:
-        params = f'?origin=edit&status={message}'
-        return redirect(f'/links/all{params}')
+
+    return redirect(f'/links/all?origin=edit&status={message}')
